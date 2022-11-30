f05902ec10bcabed1508627b59f4f007e5b80d3a
Dop <dopddaiga@gmail.com>
2022-11-30 13:17:27 +0100

    Create Core app

--- app/app/test.py
+++ app/app/test.py
@@ -3,20 +3,20 @@ sample test
 """
 from django.test import SimpleTestCase
 
+
 from app import calc
 
-class  CalcTest(SimpleTestCase):
+
+class CalcTest(SimpleTestCase):
     """ Test the calc module"""
 
     def test_add_numbers(self):
         """ test adding numbers """
         res = calc.add(5, 6)
-
         self.assertEqual(res, 11)
     
     # TDD
     def test_subtract_numbers(self):
         """ Test subtracting numbers """
         res = calc.subtract(10, 15)
-
         self.assertEqual(res, 5)

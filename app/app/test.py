2b96ec699974050bcfa2ad5c710bb694a67e960f
Dop <dopddaiga@gmail.com>
2022-11-30 13:12:56 +0100

    Create Core app

--- app/app/test.py
+++ app/app/test.py
@@ -5,12 +5,12 @@ from django.test import SimpleTestCase
 
 from app import calc
 
-class CalcTest(SimpleTestCase):
+class  CalcTest(SimpleTestCase):
     """ Test the calc module"""
 
     def test_add_numbers(self):
         """ test adding numbers """
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
     

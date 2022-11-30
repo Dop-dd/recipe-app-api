cfb814ec9f38646c41f846761bc8fbccec97b495
Dop <dopddaiga@gmail.com>
2022-11-30 13:40:01 +0100

    Add wait_for_DB command

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,10 @@
+"""
+ django command to wait for thr DB to be available
+"""
+from django.core.management import BaseCommand
+
+class Command(BaseCommand):
+    """ django command to wait for thr DB """
+
+    def handle(Self, *args, **options):
+        pass
